d78d7115a84a2e2db74bd8f7f12c53879b288ed0
Lev Izraelit <leo2002b@yahoo.com>
2019-12-07 20:00:30 -0500

    use .env for local giphy api key

--- lambda/stickers/stickers.js
+++ lambda/stickers/stickers.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const axios = require("axios");
 
 const GIPHY_API_HOST = "https://api.giphy.com";
@@ -26,6 +27,8 @@ const STICKER_API_BASE_URL =
 const handler = async event => {
   try {
     const { q, offset = 0 } = event.queryStringParameters;
+    console.log("env: ", process.env.REACT_APP_GIPHY_API_KEY);
+
     const stickers = await axios.get(
       STICKER_API_BASE_URL + `&q=${q}&offset=${offset}`
     );
@@ -33,12 +36,16 @@ const handler = async event => {
     return {
       statusCode: 200,
       body: JSON.stringify(stickers.data)
-      // // more keys you can return:
-      // headers: { "headerName": "headerValue", ... },
-      // isBase64Encoded: true,
     };
-  } catch (err) {
-    return { statusCode: 500, body: err.toString() };
+  } catch (error) {
+    if (error.response) {
+      const { status, data = {} } = error.response;
+      const { message = "error fetching stickers" } = data;
+      return { statusCode: status, body: message };
+    } else if (error.request) {
+      return { statusCode: 500, body: "no response received from giphy api" };
+    }
+    return { statusCode: 500, body: error.message };
   }
 };
 
